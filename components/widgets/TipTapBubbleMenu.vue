710bbad5571a03fe7191c964acb2bdab4da4aa4d
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2025-07-30 23:15:04 +0300

    upd: tiptap

--- components/widgets/TipTapBubbleMenu.vue
+++ components/widgets/TipTapBubbleMenu.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import type { Editor } from "@tiptap/vue-3";
 import { BubbleMenu } from "@tiptap/vue-3/menus";
+
 interface Action {
   name: string;
   label: string;
   icon: string;
   command: () => void;
+  isActive: () => boolean;
 }
 
 const { t } = useI18n();
@@ -20,72 +22,84 @@ const textActions: Action[] = [
     label: t("widgets.tiptap.bold"),
     icon: "i-lucide-bold",
     command: () => props.editor?.chain().focus().toggleBold().run(),
+    isActive: () => props.editor?.isActive("bold") || false,
   },
   {
     name: "italic",
     label: t("widgets.tiptap.italic"),
     icon: "i-lucide-italic",
     command: () => props.editor?.chain().focus().toggleItalic().run(),
+    isActive: () => props.editor?.isActive("italic") || false,
   },
   {
     name: "strike",
     label: t("widgets.tiptap.strikethrough"),
     icon: "i-lucide-strikethrough",
     command: () => props.editor?.chain().focus().toggleStrike().run(),
+    isActive: () => props.editor?.isActive("strike") || false,
   },
   {
     name: "underline",
     label: t("widgets.tiptap.underline"),
     icon: "i-lucide-underline",
     command: () => props.editor?.chain().focus().toggleUnderline().run(),
+    isActive: () => props.editor?.isActive("underline") || false,
   },
   {
     name: "bulletList",
     label: t("widgets.tiptap.unordered-list"),
     icon: "i-lucide-list",
     command: () => props.editor?.chain().focus().toggleBulletList().run(),
+    isActive: () => false,
   },
   {
     name: "orderedList",
     label: t("widgets.tiptap.ordered-list"),
     icon: "i-lucide-list-ordered",
     command: () => props.editor?.chain().focus().toggleOrderedList().run(),
+    isActive: () => false,
   },
   {
     name: "alignLeft",
     label: t("widgets.tiptap.align.left"),
     icon: "i-lucide-align-left",
     command: () => props.editor?.chain().focus().setTextAlign("left").run(),
+    isActive: () => props.editor?.isActive({ textAlign: "left" }) || false,
   },
   {
     name: "alignCenter",
     label: t("widgets.tiptap.align.center"),
     icon: "i-lucide-align-center",
     command: () => props.editor?.chain().focus().setTextAlign("center").run(),
+    isActive: () => props.editor?.isActive({ textAlign: "center" }) || false,
   },
   {
     name: "alignRight",
     label: t("widgets.tiptap.align.right"),
     icon: "i-lucide-align-right",
     command: () => props.editor?.chain().focus().setTextAlign("right").run(),
+    isActive: () => props.editor?.isActive({ textAlign: "right" }) || false,
   },
   {
     name: "alignJustify",
     label: t("widgets.tiptap.align.justify"),
     icon: "i-lucide-align-justify",
     command: () => props.editor?.chain().focus().setTextAlign("justify").run(),
+    isActive: () => props.editor?.isActive({ textAlign: "justify" }) || false,
   },
   {
     name: "undo",
     label: t("widgets.tiptap.undo"),
     icon: "i-lucide-undo",
     command: () => props.editor?.chain().focus().undo().run(),
+    isActive: () => false,
   },
   {
     name: "redo",
     label: t("widgets.tiptap.redo"),
     icon: "i-lucide-redo",
     command: () => props.editor?.chain().focus().redo().run(),
+    isActive: () => false,
   },
   {
     name: "link",
@@ -95,6 +109,7 @@ const textActions: Action[] = [
       const url = prompt("Enter URL");
       if (url) props.editor?.chain().focus().setLink({ href: url }).run();
     },
+    isActive: () => props.editor?.isActive("link") || false,
   },
 ];
 
@@ -104,36 +119,42 @@ const contentTypes: Action[] = [
     label: t("widgets.tiptap.paragraph"),
     icon: "i-lucide-letter-text",
     command: () => props.editor?.chain().focus().setParagraph().run(),
+    isActive: () => false,
   },
   {
     name: "heading",
     label: t("widgets.tiptap.heading") + " 1",
     icon: "i-lucide-heading-1",
     command: () => props.editor?.chain().focus().toggleHeading({ level: 1 }).run(),
+    isActive: () => false,
   },
   {
     name: "heading",
     label: t("widgets.tiptap.heading") + " 2",
     icon: "i-lucide-heading-2",
     command: () => props.editor?.chain().focus().toggleHeading({ level: 2 }).run(),
+    isActive: () => false,
   },
   {
     name: "heading",
     label: t("widgets.tiptap.heading") + " 3",
     icon: "i-lucide-heading-3",
     command: () => props.editor?.chain().focus().toggleHeading({ level: 3 }).run(),
+    isActive: () => false,
   },
   {
     name: "codeBlock",
     label: t("widgets.tiptap.code"),
     icon: "i-lucide-code",
     command: () => props.editor?.chain().focus().toggleCodeBlock().run(),
+    isActive: () => false,
   },
   {
     name: "blockquote",
     label: t("widgets.tiptap.blockquote"),
     icon: "i-lucide-quote",
     command: () => props.editor?.chain().focus().toggleBlockquote().run(),
+    isActive: () => false,
   },
 ];
 
@@ -191,7 +212,7 @@ const clearTextColor = () => {
         </template>
       </UPopover>
       <UTooltip v-for="action in textActions" :key="action.name" :text="action.label" :popper="{ placement: 'top' }">
-        <UButton variant="ghost" :icon="action.icon" @click="action.command()" />
+        <UButton :variant="action.isActive() ? 'soft' : 'ghost'" :icon="action.icon" @click="action.command()" />
       </UTooltip>
 
       <UPopover>
@@ -200,8 +221,12 @@ const clearTextColor = () => {
           <div class="grid grid-cols-5 gap-2 p-2 bg-zinc-100 dark:bg-zinc-800 mt-2 rounded-md shadow-sm">
             <UTooltip v-for="color in colors" :key="color.value" :text="color.name" :popper="{ placement: 'top' }">
               <button
-                class="w-6 h-6 rounded-full border border-zinc-300 dark:border-zinc-600"
-                :class="color.value"
+                class="w-6 h-6 rounded-full border-2"
+                :class="{
+                  'border-primary': props.editor?.getAttributes('textStyle')?.color === color.value,
+                  'border-zinc-300 dark:border-zinc-600':
+                    props.editor?.getAttributes('textStyle')?.color !== color.value,
+                }"
                 :style="{ backgroundColor: color.value }"
                 @click="setTextColor(color.value)"
               />
